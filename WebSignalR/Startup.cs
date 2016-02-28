167c8f8c09286b9afc3a4a7534a7d50a274f3ef9
Rammun <russslllan@mail.ru>
2016-02-28 17:33:48 +0300

    Настроил регистрацию. Подоготовил под бой страницу.

---
+++ WebSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSignalR.Startup))]
+namespace WebSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
