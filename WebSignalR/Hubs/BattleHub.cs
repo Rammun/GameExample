df3aa67a2c146c2da9d37f727aa91766e2c7e3ce
Rammun <russslllan@mail.ru>
2016-03-10 14:27:37 +0300

    делал непонять чего

--- WebSignalR/Hubs/BattleHub.cs
+++ WebSignalR/Hubs/BattleHub.cs
@@ -10,6 +10,7 @@ namespace WebSignalR.Hubs
     public class BattleHub : Hub
     {
         static List<UserParam> Users = new List<UserParam>();
+        static Random rnd = new Random();
 
         // Подключение нового пользователя
         public void Connect(string userName)
@@ -28,9 +29,22 @@ namespace WebSignalR.Hubs
             }
         }
 
-        public void AddHit(UserParam user1, UserParam user2)
+        public void AddHit(UserParam user1, UserParam user2, string hit)
         {
-            Clients.Client(user2.Id).addHit(user1, user2);
+            SetNewParam(user1, user2, hit);
+            Clients.Client(user2.Id).addHit(user2, user1);
+        }
+
+        private void SetNewParam(UserParam user1, UserParam user2, string hit)
+        {
+            int[] hits = user1.skills[hit];
+            int damage = rnd.Next(hits[0], hits[1]);
+
+            if (user1.MP >= hits[2] && user2.HP > 0)
+            {
+                user2.HP -= damage;
+                user1.MP -= hits[2];
+            }
         }
     }
 }
